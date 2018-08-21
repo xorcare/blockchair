e2f775dea16eb151c90ca626c54b48534970f215
Vasiliy Vasilyuk <vasilyuk.vasiliy@gmail.com>
2018-08-21 12:59:52 +0300

    doc: Added doc file and header

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2018 Vasiliy Vasilyuk. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package blockchair Golang client Blockchair api -> https://github.com/Blockchair/Blockchair.Support/blob/master/API.md
+*/
+package blockchair
